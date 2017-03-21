e0981dad017dc978e7b881ceb75a31d1644ea38a
No Name <SwedishBotMafia@users.noreply.github.com>
2017-03-21 11:04:57 -0500

    Update pdf_parser.py
    this actually works.

--- textract/parsers/pdf_parser.py
+++ textract/parsers/pdf_parser.py
@@ -1,7 +1,5 @@
 import os
-import shutil
-import six
-from tempfile import mkdtemp
+from tempfile import mkdtemp, mkstemp
 
 from ..exceptions import UnknownMethod, ShellError
 
@@ -17,49 +15,67 @@ class Parser(ShellParser):
     def extract(self, filename, method='', **kwargs):
         if method == '' or method == 'pdftotext':
             try:
-                return self.extract_pdftotext(filename, **kwargs)
+                return self.extract_pdftotext(filename)
             except ShellError as ex:
                 # If pdftotext isn't installed and the pdftotext method
                 # wasn't specified, then gracefully fallback to using
                 # pdfminer instead.
-                if method == '' and ex.is_not_installed():
-                    return self.extract_pdfminer(filename, **kwargs)
+                if method == '' and ex.is_uninstalled():
+                    return self.extract_pdfminer(filename)
                 else:
                     raise ex
 
         elif method == 'pdfminer':
-            return self.extract_pdfminer(filename, **kwargs)
+            return self.extract_pdfminer(filename)
         elif method == 'tesseract':
-            return self.extract_tesseract(filename, **kwargs)
+            return self.extract_tesseract(filename)
         else:
             raise UnknownMethod(method)
 
-    def extract_pdftotext(self, filename, **kwargs):
+    def extract_pdftotext(self, filename):
         """Extract text from pdfs using the pdftotext command line utility."""
-        if 'layout' in kwargs:
-            args = ['pdftotext', '-layout', filename, '-']
-        else:
-            args = ['pdftotext', filename, '-']
-        stdout, _ = self.run(args)
+        stdout, _ = self.run('pdftotext "%(filename)s" -' % locals())
         return stdout
 
-    def extract_pdfminer(self, filename, **kwargs):
+    def convert_pdf_to_txt(self, path):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = file(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos = set()
+
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+
+        text = retstr.getvalue()
+
+        fp.close()
+        device.close()
+        retstr.close()
+        return text
+
+    def extract_pdfminer(self, filename):
         """Extract text from pdfs using pdfminer."""
-        stdout, _ = self.run(['pdf2txt.py', filename])
+        # stdout, _ = self.run('pdf2txt.py "%(filename)s"' % locals())
+        stdout = self.convert_pdf_to_txt(filename)
         return stdout
 
-    def extract_tesseract(self, filename, **kwargs):
+    def extract_tesseract(self, filename):
         """Extract text from pdfs using tesseract (per-page OCR)."""
         temp_dir = mkdtemp()
         base = os.path.join(temp_dir, 'conv')
-        contents = []
-        try:
-            stdout, _ = self.run(['pdftoppm', filename, base])
+        stdout, _ = self.run('pdftoppm "%s" "%s"' % (filename, base))
 
-            for page in sorted(os.listdir(temp_dir)):
-                page_path = os.path.join(temp_dir, page)
-                page_content = TesseractParser().extract(page_path, **kwargs)
-                contents.append(page_content)
-            return six.b('').join(contents)
-        finally:
-            shutil.rmtree(temp_dir)
+        contents = []
+        for page in os.listdir(temp_dir):
+            page_path = os.path.join(temp_dir, page)
+            page_content = TesseractParser().extract(page_path)
+            contents.append(page_content)
+        return '\n\n'.join(contents)
